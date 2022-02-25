208606d655aadd5078903c8ee9de38532886f5c0
Yukimai <yukimaikoriya@vip.qq.com>
2022-02-25 14:03:16 +0800

    Disable cfc when -flto is on

--- gcc/cfc.cc
+++ gcc/cfc.cc
@@ -110,7 +110,10 @@ static int get_func_cfc_attr(tree func_decl)
 static bool is_cfc_enabled(function *func)
 {
   int attr = get_func_cfc_attr(func->decl);
-  return global_options.x_cfc_enable ? attr != CFC_ATTR_DISABLE : attr == CFC_ATTR_ENABLE;
+  bool ret = global_options.x_cfc_enable ? attr != CFC_ATTR_DISABLE : attr == CFC_ATTR_ENABLE;
+  if(ret && global_options.x_flag_lto)
+    warning(OPT_Wpragmas, "CF Check is disabled because %<-flto%> is present");
+  return ret && !global_options.x_flag_lto;
 }
 
 unsigned int pass_cfcss::execute(function *fun) {
